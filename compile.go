8c04a6d26f7f06c427442dd037fa3f1db41de450
Ilia Choly <icholy@accipiterradar.com>
2018-11-05 14:04:14 -0500

    split up code

--- compile.go
+++ compile.go
@@ -1,116 +1,9 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 	"strings"
 )
-
-type Token string
-
-func (t Token) String() string { return string(t) }
-
-const (
-	GT      = Token("GT")
-	LT      = Token("LT")
-	PLUS    = Token("PLUS")
-	SUB     = Token("SUB")
-	DOT     = Token("DOT")
-	COMMA   = Token("COMMA")
-	OPEN    = Token("OPEN")
-	CLOSE   = Token("CLOSE")
-	EOF     = Token("EOF")
-	INVALID = Token("INVALID")
-)
-
-var mapping = map[rune]Token{
-	'>': GT,
-	'<': LT,
-	'+': PLUS,
-	'-': SUB,
-	'.': DOT,
-	'[': OPEN,
-	']': CLOSE,
-	',': COMMA,
-}
-
-func Tokenize(s string) []Token {
-	var tokens []Token
-	for _, r := range s {
-		switch r {
-		case ' ', '\n', '\r', '\t':
-		default:
-			tok, ok := mapping[r]
-			if !ok {
-				tok = INVALID
-			}
-			tokens = append(tokens, tok)
-		}
-	}
-	return append(tokens, EOF)
-}
-
-type Node interface{ fmt.Stringer }
-
-type Op Token
-
-func (o Op) String() string { return string(o) }
-
-type Loop []Node
-
-func (l Loop) String() string {
-	nodes := make([]string, len(l))
-	for i, n := range l {
-		nodes[i] = n.String()
-	}
-	return fmt.Sprintf("[%s]", strings.Join(nodes, ", "))
-}
-
-type Program []Node
-
-func (p Program) String() string {
-	nodes := make([]string, len(p))
-	for i, n := range p {
-		nodes[i] = n.String()
-	}
-	return strings.Join(nodes, ", ")
-}
-
-func ParseNodes(pos int, tokens []Token) ([]Node, int, error) {
-	var nodes []Node
-	for pos = pos; pos < len(tokens); pos++ {
-		tok := tokens[pos]
-		switch tok {
-		case INVALID:
-			// ignore invalid tokens
-		case EOF:
-			return nodes, pos, nil
-		case CLOSE:
-			return nodes, pos, nil
-		case OPEN:
-			lnodes, lpos, err := ParseNodes(pos+1, tokens)
-			if err != nil {
-				return nil, lpos, err
-			}
-			if tok := tokens[lpos]; tok != CLOSE {
-				return nil, pos, fmt.Errorf("expected %s, got %s", CLOSE, tok)
-			}
-			pos = lpos
-			nodes = append(nodes, Loop(lnodes))
-		default:
-			nodes = append(nodes, Op(tok))
-		}
-	}
-	return nil, 0, fmt.Errorf("missing %s", EOF)
-}
-
-func Parse(tokens []Token) (Program, error) {
-	nodes, _, err := ParseNodes(0, tokens)
-	return Program(nodes), err
-}
 
 type Labels struct {
 	count int
@@ -208,7 +101,6 @@ func CompileCleanup(labels *Labels) []string {
 	}
 }
 
-
 func fmtIns(format string, args ...interface{}) string {
 	return fmt.Sprintf(format, args...)
 }
@@ -258,28 +150,4 @@ func CompileNode(node Node, labels *Labels) ([]string, error) {
 func Compile(p Program) ([]string, error) {
 	var labels Labels
 	return CompileNode(p, &labels)
-}
-
-func main() {
-	var infile, outfile string
-	flag.StringVar(&infile, "f", "", "input file")
-	flag.StringVar(&outfile, "o", "out.asm", "output file")
-	flag.Parse()
-	src, err := ioutil.ReadFile(infile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tokens := Tokenize(string(src))
-	program, err := Parse(tokens)
-	if err != nil {
-		log.Fatal(err)
-	}
-	instructions, err := Compile(program)
-	if err != nil {
-		log.Fatal(err)
-	}
-	assembly := strings.Join(instructions, "\n")
-	if err := ioutil.WriteFile(outfile, []byte(assembly), os.ModePerm); err != nil {
-		log.Fatal(err)
-	}
 }
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	var infile, outfile string
+	flag.StringVar(&infile, "f", "", "input file")
+	flag.StringVar(&outfile, "o", "out.asm", "output file")
+	flag.Parse()
+	src, err := ioutil.ReadFile(infile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tokens := Tokenize(string(src))
+	program, err := Parse(tokens)
+	if err != nil {
+		log.Fatal(err)
+	}
+	instructions, err := Compile(program)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assembly := strings.Join(instructions, "\n")
+	if err := ioutil.WriteFile(outfile, []byte(assembly), os.ModePerm); err != nil {
+		log.Fatal(err)
+	}
+}
